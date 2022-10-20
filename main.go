c5efa65a05ca819af9eed22dd70606cb61912430
Raul França <raulmfranca@gmail.com>
2022-10-20 09:38:07 -0300

    Base do projeto

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", func(wri http.ResponseWriter, req *http.Request) {
+		wri.Write([]byte(`
+			<html>
+			   <head>
+				 <title>Chat</title>
+			   </head>
+			   <body>
+				 Let's chat!
+			   </body>
+			</html>
+		`))
+	})
+	//Inicia o web service
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
